482757f133e171b84c01548bc291a8c9ab88fd19
bala-amypo <bala@amypo.in>
2025-12-17 09:39:58 +0000

    User commit

--- src/main/java/com/example/demo/Serviceimpl/Stuserviceimpl.java
+++ src/main/java/com/example/demo/Serviceimpl/Stuserviceimpl.java
@@ -1,10 +1,16 @@
 package com.example.demo.serviceimpl;
 
-import com.example.demo.service.service;
 
+import com.example.demo.service.service;
+import com.example.demo.entity.Sturepository;
+import com.example.demo.repository.Sturepository;
 import com.example.demo.service.Stuservice;
 
 @service
 public class Stuserviceimpl implements Stuservice{
-    
+
+    public Stuentity saveStudent(Stuentity student){
+
+    }
+
 } 
